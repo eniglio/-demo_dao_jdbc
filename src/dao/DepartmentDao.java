4266a5d893ab6dfdde6cbd785c2e5a5581318a01
eniglio <eniglio@hotmail.com>
2025-06-03 14:58:13 -0300

    Full implementation

--- src/dao/DepartmentDao.java
+++ src/dao/DepartmentDao.java
@@ -7,7 +7,7 @@ import entities.Department;
 public interface DepartmentDao {
 	void insert(Department obj);
 	void update(Department obj);
-	void deletById(Integer id);
+	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
 }
