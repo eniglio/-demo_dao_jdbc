5df7f699c9e63a3ea526857fbdb5eedfb6e50d09
eniglio <eniglio@hotmail.com>
2025-06-03 10:39:43 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,8 @@ public class Program {
  		
 		//System.out.println(seller);
 
+		System.out.println("===TEST 1: seller findById ====");
+		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
